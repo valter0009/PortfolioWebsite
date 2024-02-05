2f2e67e1b90300facefb6b4d0e3e5a8002f167a5
Ilya Kudryavtsev <75815095+valter0009@users.noreply.github.com>
2024-02-05 22:56:04 +0100

    configuring http

--- PortfolioWebsite.App/Program.cs
+++ PortfolioWebsite.App/Program.cs
@@ -37,11 +37,11 @@ builder.Services.AddScoped(sp =>
 {
 	NavigationManager navigation = sp.GetRequiredService<NavigationManager>();
 	return new HttpClient { BaseAddress = new Uri(navigation.BaseUri) };
-}); //https://localhost:7240
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
